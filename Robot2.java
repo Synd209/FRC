cad8dae6e93d1af6623b1075691e641a03fb152c
Synd209 <kutaydemirbas2009@gmail.com>
2024-11-16 17:16:15 +0300

    kaydetmeyi unutmuşum lmao

--- Robot2.java
+++ Robot2.java
@@ -1,21 +1,34 @@
 package frc.robot;
-import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
-public class Robot extends TimedRobot {
-  DifferentialDrive __drive;
-  Joystick __joystick;
+public class Robot2 extends TimedRobot {
+    PWMSparkMax __arm_motor;
+    Encoder __arm_encoder;
+    PIDController __pid_controller;
 
-  @Override
-  public void robotInit() {
-    __drive = new DifferentialDrive(new PWMSparkMax(0), new PWMSparkMax(1));
-    __joystick = new Joystick(0);
-  }
+    static final double kP = 0.05;
+    static final double kI = 0.0;
+    static final double kD = 0.01;
+    static final double __target__ang = 90.0; // Hedef açı (derece)
+    static final double __dist_per_pulse = 0.15;
 
-  @Override
-  public void teleopPeriodic() {
-    __drive.arcadeDrive(-__joystick.getY(), __joystick.getX());
-  }
+    @Override
+    public void robotInit() {
+        __arm_motor = new PWMSparkMax(2);
+        __arm_encoder = new Encoder(0, 1);
+
+        __arm_encoder.setDistancePerPulse(__dist_per_pulse);
+
+        __pid_controller = new PIDController(kP, kI, kD);
+        __pid_controller.setSetpoint(__target__ang); // Hedef açıyı belirle
+    }
+
+    @Override
+    public void teleopPeriodic() {
+      double currentAngle = __arm_encoder.getDistance();
+      __arm_motor.set(__pid_controller.calculate(currentAngle));
+    }
 }
